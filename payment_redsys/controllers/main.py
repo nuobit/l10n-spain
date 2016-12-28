ef19579a4851e43dbdf1191a624c684d3bd5fae1
Eric Antones <eantones@nuobit.com>
2016-12-28 11:49:33 +0100

    Revert "[FIX] payment_redsys: remove order summary after payment when the user is not authenticated"
    This reverts commit 97d9d93130e6eedfef977075e7bc57b71fa4c8b6.

--- payment_redsys/controllers/main.py
+++ payment_redsys/controllers/main.py
@@ -36,18 +36,16 @@ class RedsysController(http.Controller):
         return werkzeug.utils.redirect(return_url)
 
     @http.route(
-        ['/payment/redsys/result/<page>'], type='http', auth='public',
+        ['/payment/redsys/result/<page>'], type='http', auth='user',
         methods=['GET'], website=True)
     def redsys_result(self, page, **vals):
         try:
-            res = {}
-            if request.env.user.id != request.env.ref('base.public_user').id:
-                order_id = vals.get('order_id', 0)
-                sale_obj = request.env['sale.order']
-                order = sale_obj.browse(int(order_id))
-                res = {
-                    'order': order,
-                }
+            order_id = vals.get('order_id', 0)
+            sale_obj = request.env['sale.order']
+            order = sale_obj.browse(int(order_id))
+            res = {
+                'order': order,
+            }
             return request.render('payment_redsys.%s' % str(page), res)
         except:
             return request.render('website.404')
