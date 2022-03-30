19017eae468d1ecf42670a0e6c399aa8db877019
Eric Antones <eantones@nuobit.com>
2022-03-30 13:25:52 +0200

    [FIX] l10n_es_aeat_sii_oca: Clave "CuotaDeducible" incorrecta en los impuestos con parte no deducible
    En los impuestos que tienen parte no deducible, no se está teniendo en cuenta dicha parte en el envío al SII, siempre se informa el campo "CuotaDeducible" con el mismo valor que el campo "CuotaSoportada"

--- l10n_es_aeat_sii_oca/models/account_move.py
+++ l10n_es_aeat_sii_oca/models/account_move.py
@@ -8,6 +8,7 @@
 # Copyright 2021 Tecnativa - João Marques
 # Copyright 2022 ForgeFlow - Lois Rilo
 # Copyright 2022 Tecnativa - Víctor Martínez
+# Copyright 2022 NuoBiT - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import json
@@ -518,7 +519,9 @@ class AccountMove(models.Model):
         for line in self.line_ids:
             sign = -1 if self.move_type[:3] == "out" else 1
             for tax in line.tax_ids:
-                res.setdefault(tax, {"tax": tax, "base": 0, "amount": 0})
+                res.setdefault(
+                    tax, {"tax": tax, "base": 0, "amount": 0, "deductible_amount": 0}
+                )
                 res[tax]["base"] += line.balance * sign
             if line.tax_line_id:
                 tax = line.tax_line_id
@@ -532,8 +535,12 @@ class AccountMove(models.Model):
                 ):
                     # taxes with more than one "tax" repartition line must be discarded
                     continue
-                res.setdefault(tax, {"tax": tax, "base": 0, "amount": 0})
+                res.setdefault(
+                    tax, {"tax": tax, "base": 0, "amount": 0, "deductible_amount": 0}
+                )
                 res[tax]["amount"] += line.balance * sign
+                if line.tax_repartition_line_id.account_id:
+                    res[tax]["deductible_amount"] += line.balance * sign
         return res
 
     def _get_sii_out_taxes(self):  # noqa: C901
@@ -681,6 +688,7 @@ class AccountMove(models.Model):
         taxes_not_in_total_neg = self._get_sii_taxes_map(["NotIncludedInTotalNegative"])
         base_not_in_total = self._get_sii_taxes_map(["BaseNotIncludedInTotal"])
         tax_amount = 0.0
+        tax_deductible_amount = 0.0
         not_in_amount_total = 0.0
         tax_lines = self._get_tax_info()
         for tax_line in tax_lines.values():
@@ -703,6 +711,7 @@ class AccountMove(models.Model):
             tax_dict = self._get_sii_tax_dict(tax_line, tax_lines)
             if tax in taxes_sfrisp + taxes_sfrs:
                 tax_amount += tax_line["amount"]
+                tax_deductible_amount += tax_line["deductible_amount"]
             if tax in taxes_sfrns:
                 tax_dict.pop("TipoImpositivo")
                 tax_dict.pop("CuotaSoportada")
@@ -719,7 +728,7 @@ class AccountMove(models.Model):
                     ["BaseImponible", "CuotaSoportada"],
                 ):
                     base_dict["DetalleIVA"].append(tax_dict)
-        return taxes_dict, tax_amount, not_in_amount_total
+        return taxes_dict, tax_amount, tax_deductible_amount, not_in_amount_total
 
     def _is_sii_simplified_invoice(self):
         """Inheritable method to allow control when an
@@ -884,7 +893,12 @@ class AccountMove(models.Model):
         ejercicio = fields.Date.to_date(self.date).year
         periodo = "%02d" % fields.Date.to_date(self.date).month
         partner = self._sii_get_partner()
-        desglose_factura, tax_amount, not_in_amount_total = self._get_sii_in_taxes()
+        (
+            desglose_factura,
+            tax_amount,
+            tax_deductible_amount,
+            not_in_amount_total,
+        ) = self._get_sii_in_taxes()
         inv_dict = {
             "IDFactura": {
                 "IDEmisorFactura": {},
@@ -911,7 +925,7 @@ class AccountMove(models.Model):
                 "Contraparte": {"NombreRazon": partner.name[0:120]},
                 "FechaRegContable": reg_date,
                 "ImporteTotal": amount_total,
-                "CuotaDeducible": tax_amount,
+                "CuotaDeducible": tax_deductible_amount,
             }
             if self.sii_macrodata:
                 inv_dict["FacturaRecibida"].update(Macrodato="S")
