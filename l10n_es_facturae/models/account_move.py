93654e96cb2f10d3fb5a68c2a9eeb9a1d1defd2d
FrankC013 <fcespedes@nuobit.com>
2024-09-30 19:03:40 +0200

    [FIX] l10n_es_facturae: rounding tax amount error (new approach)

--- l10n_es_facturae/models/account_move.py
+++ l10n_es_facturae/models/account_move.py
@@ -224,6 +224,12 @@ class AccountMove(models.Model):
                 else:
                     withheld_taxes[tax]["base"] += base
                     withheld_taxes[tax]["amount"] += tax_amount
+        if self.company_id.tax_calculation_rounding_method == "round_globally":
+            for taxes in (output_taxes, withheld_taxes):
+                for tax, values in taxes.items():
+                    values["amount"] = tax.get_tax_amount(
+                        values["base"], self.currency_id, self.company_id
+                    )
         return output_taxes, withheld_taxes
 
 
@@ -274,6 +280,41 @@ class AccountMoveLine(models.Model):
             "res_id": self.id,
             "context": self.env.context,
         }
+
+    def _get_facturae_tax_line_info(self, tax):
+        self.ensure_one()
+        tax_line_info = {}
+        sum_tax_amount = 0.0
+
+        for line in self.move_id.line_ids.filtered(
+            lambda x: not x.display_type and not x.exclude_from_invoice_tab
+        ):
+            for line_tax in line.tax_ids.filtered(lambda t: t == tax and t.amount >= 0):
+                tax_amount = tax.get_tax_amount(
+                    line.price_subtotal, self.currency_id, self.company_id
+                )
+                tax_line_info.setdefault((line, line_tax), tax_amount)
+                sum_tax_amount += tax_amount
+
+        move_tax_line = self.move_id.line_ids.filtered(lambda x: x.tax_line_id == tax)
+        total_tax_line_tax_amount = self.currency_id.round(
+            sum(move_tax_line.mapped("balance"))
+        )
+        diff_tax_amount = self.currency_id.round(sum_tax_amount) - (
+            -total_tax_line_tax_amount
+        )
+
+        if diff_tax_amount != 0:
+            sorted_lines = sorted(tax_line_info.keys(), key=lambda k: k[0].id)
+            num_lines_to_adjust = min(
+                len(sorted_lines), int(abs(diff_tax_amount * 100))
+            )
+            adjustment_value = 0.01 if diff_tax_amount > 0 else -0.01
+            for i in range(num_lines_to_adjust):
+                line, line_tax = sorted_lines[i]
+                tax_line_info[(line, line_tax)] += adjustment_value
+
+        return tax_line_info[(self, tax)]
 
 
 class L10nEsFacturaeAttachment(models.Model):
--- l10n_es_facturae/models/account_tax.py
+++ l10n_es_facturae/models/account_tax.py
@@ -2,10 +2,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
+from odoo.tools.float_utils import float_round as round
 
 
 class AccountTax(models.Model):
     _inherit = "account.tax"
+
+    def get_tax_amount(self, amount, currency, company):
+        self.ensure_one()
+        tax_amount = amount * self.amount / 100
+        prec = currency.rounding
+        if company.tax_calculation_rounding_method == "round_globally":
+            prec *= 1e-5
+        tax_amount = round(tax_amount, precision_rounding=prec)
+        return tax_amount
 
     facturae_code = fields.Selection(
         selection=[
